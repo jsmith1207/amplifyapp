a1b5d1717c75497eb5f9ab126b25c4e071926467
jsmith1207 <js0131@mix.wvu.edu>
2023-12-15 02:12:53 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 03</h2>
+      <p>WVU ID: 800241956</p>
+      <p>Hi, I am Joshua Smith</p>
+    </div>
+  );
+}
+
+export default App;
